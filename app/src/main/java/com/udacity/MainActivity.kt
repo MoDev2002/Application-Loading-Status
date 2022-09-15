08271a404cf139fd98a5c795f776627385fa16d6
Mohamed El Masry <momamedalmasry@gmail.com>
2022-09-15 14:26:59 +0200

    Adding Notifications

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -25,18 +30,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var fileName : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        createChannel(getString(R.string.notification_channel_id), getString(R.string.notification_channel_name))
 
         custom_button.setOnClickListener {
             when(findViewById<RadioButton>(radioGroup.checkedRadioButtonId)) {
-                glide -> download(GLIDE_URL, R.string.glide_title, R.string.glide_description)
-                retrofit -> download(RETROFIT_URL, R.string.retrofit_title, R.string.retrofit_description)
-                loadApp -> download(APP_URL, R.string.loadapp_title, R.string.loadapp_description)
+                glide -> {
+                    fileName = getString(R.string.glide_text)
+                    download(GLIDE_URL, R.string.glide_title, R.string.glide_description)
+                }
+                retrofit -> {
+                    fileName = getString(R.string.retrofit_text)
+                    download(RETROFIT_URL, R.string.retrofit_title, R.string.retrofit_description)
+                }
+                loadApp -> {
+                    fileName = getString(R.string.loadapp_text)
+                    download(APP_URL, R.string.loadapp_title, R.string.loadapp_description)
+                }
                 else -> Toast.makeText(this, "Please Select File To Download", Toast.LENGTH_SHORT).show()
             }
         }
@@ -46,6 +63,28 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
             custom_button.buttonState = ButtonState.Completed
+
+            var downloadStatus = "Fail"
+
+            val downloadManager = getSystemService(DownloadManager::class.java)
+
+            val query = DownloadManager.Query()
+            query.setFilterById(id!!)
+            val cursor = downloadManager.query(query)
+            if(cursor.moveToFirst()){
+                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+
+                if(DownloadManager.STATUS_SUCCESSFUL == status){
+                    downloadStatus = "Success"
+                }
+            }
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.sendNotification(
+                getString(R.string.notification_description),
+                applicationContext,
+                downloadStatus,
+                fileName
+            )
         }
     }
 
@@ -71,6 +110,24 @@ class MainActivity : AppCompatActivity() {
         private const val GLIDE_URL = "https://github.com/bumptech/glide"
         private const val RETROFIT_URL = "https://codeload.github.com/square/retrofit/zip/refs/heads/master"
         private const val CHANNEL_ID = "channelId"
+    }
+
+    private fun createChannel(channelId : String, channelName : String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.BLUE
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "File download status"
+
+            val notificationManager = this.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/udacity/NotificationUtils.kt
@@ -0,0 +1,38 @@
+package com.udacity
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+
+const val NOTIFICATION_ID = 0
+const val INTENT_STATUS_KEY = "status"
+const val INTENT_FILE_KEY = "file_name"
+
+fun NotificationManager.sendNotification(messageBody : String, applicationContext: Context, status : String, fileName : String) {
+
+    val detailScreenIntent = Intent(applicationContext, DetailActivity::class.java)
+    detailScreenIntent.putExtra(INTENT_STATUS_KEY, status)
+    detailScreenIntent.putExtra(INTENT_FILE_KEY, fileName)
+
+    val buttonIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        detailScreenIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+
+    val builder = NotificationCompat.Builder(applicationContext, applicationContext.getString(R.string.notification_channel_id))
+        .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+        .setContentTitle(applicationContext.getString(R.string.button_text))
+        .setContentText(messageBody)
+        .setAutoCancel(true)
+        .addAction(
+            R.drawable.ic_assistant_black_24dp,
+            "details"
+            ,buttonIntent)
+        .setPriority(NotificationCompat.PRIORITY_MAX)
+
+    notify(NOTIFICATION_ID, builder.build())
+}
